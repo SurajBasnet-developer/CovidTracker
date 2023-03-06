53b3a1475cad8df00fe4e3f526c8973dd5b2ce0b
Suraj <surajbasnet449@gmail.com>
2023-03-06 17:43:17 +0545

    first commit

---
+++ src/Covid.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+const Covid = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://disease.sh/v3/covid-19/all");
+      const responseData = await response.json();
+      setData(responseData);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h3>Covid-19 Tracker</h3>
+      </div>
+      {data ? (
+        <div className="data-container">
+          <div className="data-card">
+            <h2>Total Cases</h2>
+            <p>{data.cases.toLocaleString()}</p> <hr></hr>
+          </div>
+          <div className="data-card">
+            <h2>Total Deaths</h2>
+            <p>{data.deaths.toLocaleString()}</p>
+            <hr></hr>
+          </div>
+          <div className="data-card">
+            <h2>Total Recovered</h2>
+            <p>{data.recovered.toLocaleString()}</p>
+            <hr></hr>
+          </div>
+
+          <div className="data-card">
+            <h2>Active Cases</h2>
+            <p>{data.active.toLocaleString()}</p>
+            <hr></hr>
+          </div>
+
+          <div className="data-card">
+            <h2>Affected Countries</h2>
+            <p>{data.affectedCountries.toLocaleString()}</p>
+            <hr></hr>
+          </div>
+
+          <div className="svg-frame"></div>
+        </div>
+      ) : (
+        <div className="loading">Loading...</div>
+      )}
+    </div>
+  );
+};
+
+export default Covid;
